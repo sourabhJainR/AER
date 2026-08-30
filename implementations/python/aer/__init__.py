a46e805ba077b0725eee89cb4e7253c8ffc376f6
Sourabh Jain <sourjain2002@gmail.com>
2026-08-30 23:18:09 +0530

    Add full Python AER implementation

---
+++ implementations/python/aer/__init__.py
@@ -0,0 +1,3 @@
+"""Reference Python implementation of AER 1.x."""
+from .core import AerValue, AerKind, encode, decode, dumps, loads
+__all__ = ["AerValue", "AerKind", "encode", "decode", "dumps", "loads"]
