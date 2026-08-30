5e9a23636e20ec424d2e70621e9f4e40a14c5f28
Sourabh Jain <sourjain2002@gmail.com>
2026-08-30 23:47:19 +0530

    Export complete Python AER parity API

--- implementations/python/aer/__init__.py
+++ implementations/python/aer/__init__.py
@@ -1,4 +1,6 @@
 """Reference Python implementation of AER 1.x."""
-from .core import AerValue, AerKind, encode, decode, dumps, loads
+from .core import AerValue, AerKind, AerTable, encode, decode, dumps, loads, table, reference
 from .schema import Field, Schema
-__all__ = ["AerValue", "AerKind", "encode", "decode", "dumps", "loads", "Field", "Schema"]
+from .patch import PatchOp, PatchOperation, apply
+from .stream import encode_frame, decode_frames
+__all__ = ["AerValue", "AerKind", "AerTable", "encode", "decode", "dumps", "loads", "table", "reference", "Field", "Schema", "PatchOp", "PatchOperation", "apply", "encode_frame", "decode_frames"]
