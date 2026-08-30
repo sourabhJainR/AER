66eda2b2d4401242e8a9f1ddc8cf19d1e2db2787
Sourabh Jain <sourjain2002@gmail.com>
2026-08-30 23:22:28 +0530

    Export Python AER schema types

--- implementations/python/aer/__init__.py
+++ implementations/python/aer/__init__.py
@@ -1,3 +1,4 @@
 """Reference Python implementation of AER 1.x."""
 from .core import AerValue, AerKind, encode, decode, dumps, loads
-__all__ = ["AerValue", "AerKind", "encode", "decode", "dumps", "loads"]
+from .schema import Field, Schema
+__all__ = ["AerValue", "AerKind", "encode", "decode", "dumps", "loads", "Field", "Schema"]
