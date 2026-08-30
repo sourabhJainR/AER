0bae2ca7ee13e725b2a794d34f4ec15fecfc42a6
Sourabh Jain <sourjain2002@gmail.com>
2026-08-30 09:44:41 +0530

    Fix ASP.NET Core result adapter API

--- src/Aer.AspNetCore/AerEndpointExtensions.cs
+++ src/Aer.AspNetCore/AerEndpointExtensions.cs
@@ -1,14 +1,13 @@
 using Aer;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Builder;
 
 namespace Aer.AspNetCore;
 
-public static class AerEndpointExtensions
+public static class AerResults
 {
-    public static IResult Aer(this IResultExtensions _, object? value) =>
+    public static IResult Text(object? value) =>
         Results.Text(AER.Serialize(value), "application/aer; profile=text", System.Text.Encoding.UTF8);
 
-    public static IResult AerAi(this IResultExtensions _, object? value) =>
+    public static IResult Ai(object? value) =>
         Results.Text(AER.ToAi(value), "application/aer; profile=ai", System.Text.Encoding.UTF8);
 }
